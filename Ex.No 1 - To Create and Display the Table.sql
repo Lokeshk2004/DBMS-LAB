e1bf49a9836dc040c0051da9ddff5e7330ee10f9
Lokesh <129819810+Lokeshk2004@users.noreply.github.com>
2023-09-24 18:26:03 +0530

    Add files via upload

---
+++ Ex.No 1 - To Create and Display the Table.sql
@@ -0,0 +1,10 @@
+--To Create a Table--
+CREATE TABLE Students
+(
+  Student_Name VARCHAR(25),
+  Student_ID VARCHAR(25),
+  Mobile_No INT,
+  Course_Name VARCHAR(25)
+);
+--To Display the Table--
+SELECT * from Students;
